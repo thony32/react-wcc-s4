06771d242a5ed992962099c65fbb8427e9901d68
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-22 19:59:59 +0300

    new component navbar mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-import { useEffect, useState } from 'react'
-import Base from './components/Base'
-import Navbar from './components/Navbar'
-
+import { useEffect, useState } from "react";
+import Base from "./components/Base";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [isLoad, setIsLoad] = useState(true)
+  const [isLoad, setIsLoad] = useState(true);
 
-  const [valia, setValia] = useState("scale-[30%] lg:translate-x-[135%] lg:translate-y-[10%] 2xl:translate-x-[200%] 2xl:translate-y-[25%] absolute duration-700")
-  const [baguette_deux, setBaguette_deux] = useState("scale-[30%] lg:translate-x-[109%] lg:translate-y-[45%] 2xl:translate-x-[160%] 2xl:translate-y-[75%] rotate-[20deg] z-10 absolute duration-700")
-  const [baguette_un, setBaguette_un] = useState("scale-[30%] lg:translate-x-[95%] lg:translate-y-[50%] translate-x-[150%] translate-y-[75%] rotate-[90deg] z-10 absolute duration-700")
-  const [vanttan, setVanttan] = useState("scale-[20%] lg:translate-x-[175%] lg:translate-y-[50%] 2xl:translate-x-[255%] 2xl:translate-y-[65%] z-0 absolute duration-700")
-
+  const [valia, setValia] = useState("scale-[30%] lg:translate-x-[135%] lg:translate-y-[10%] 2xl:translate-x-[200%] 2xl:translate-y-[25%] absolute duration-700");
+  const [baguette_deux, setBaguette_deux] = useState("scale-[30%] lg:translate-x-[109%] lg:translate-y-[45%] 2xl:translate-x-[160%] 2xl:translate-y-[75%] rotate-[20deg] z-10 absolute duration-700");
+  const [baguette_un, setBaguette_un] = useState("scale-[30%] lg:translate-x-[95%] lg:translate-y-[50%] translate-x-[150%] translate-y-[75%] rotate-[90deg] z-10 absolute duration-700");
+  const [vanttan, setVanttan] = useState("scale-[20%] lg:translate-x-[175%] lg:translate-y-[50%] 2xl:translate-x-[255%] 2xl:translate-y-[65%] z-0 absolute duration-700");
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoad(false)
-      setValia(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[145%] lg:translate-y-[10%] 2xl:translate-x-[200%] 2xl:translate-y-[25%] absolute duration-700")
-      setBaguette_deux(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[115%] lg:translate-y-[20%] 2xl:translate-x-[165%] 2xl:translate-y-[30%] z-10 absolute duration-700")
-      setBaguette_un(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[117%] lg:translate-y-[27%] translate-x-[168%] translate-y-[40%] z-10 absolute duration-700")
-      setVanttan("lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[190%] lg:translate-y-[23%] translate-x-[260%] translate-y-[45%] z-0 absolute duration-700")
-    }, 2000)
-  })
+      setIsLoad(false);
+      setValia(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[145%] lg:translate-y-[10%] 2xl:translate-x-[200%] 2xl:translate-y-[25%] absolute duration-700");
+      setBaguette_deux(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[115%] lg:translate-y-[20%] 2xl:translate-x-[165%] 2xl:translate-y-[30%] z-10 absolute duration-700");
+      setBaguette_un(" lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[117%] lg:translate-y-[27%] translate-x-[168%] translate-y-[40%] z-10 absolute duration-700");
+      setVanttan("lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[190%] lg:translate-y-[23%] translate-x-[260%] translate-y-[45%] z-0 absolute duration-700");
+    }, 2000);
+  });
 
   return (
-    <div className='gilroy relative'>
-      <div className={
-        isLoad ? 'opacity-0 duration-1000 ease-in-out' : 'opacity-100 duration-1000 ease-in-out absolute top-0 right-0 h-full -z-50'
-      }>
+    <div className="gilroy relative">
+      <div className={isLoad ? "opacity-0 duration-1000 ease-in-out" : "opacity-100 duration-1000 ease-in-out absolute top-0 right-0 h-full -z-50"}>
         <img className="h-full w-full z-0" src="/images/back.png" alt="" />
       </div>
 
       {/* images */}
       <div className="absolute -z-50 top-0 left-0 w-full h-screen">
-        <div className='h-full relative'>
-          <div className={isLoad ? '-translate-x-[23%] duration-700' : 'translate-x-0 duration-700'}>
+        <div className="h-full relative">
+          <div className={isLoad ? "-translate-x-[23%] duration-700" : "translate-x-0 duration-700"}>
             {/* vilia */}
             <img className={valia} src="/images/vilia.png" alt="" />
             {/* bagette */}
@@ -43,9 +39,7 @@ function App() {
             <img className={vanttan} src="/images/Vanttan.png" alt="" />
 
             {/* Others */}
-            <div className={
-              isLoad ? 'opacity-0 duration-1000 ease-in-out' : 'opacity-100 duration-1000 ease-in-out'
-            }>
+            <div className={isLoad ? "opacity-0 duration-1000 ease-in-out" : "opacity-100 duration-1000 ease-in-out"}>
               <img className=" blur-sm lg:scale-[55%] 2xl:scale-[100%] lg:translate-x-[545%]  lg:translate-y-[90%] 2xl:translate-x-[700%]  2xl:translate-y-[125%] absolute duration-700" src="/images/tomate.png" alt="" />
               <img className=" blur-sm lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[520%]  lg:translate-y-[50%] translate-x-[700%]  translate-y-[75%] absolute duration-700" src="/images/leaf 1.png" alt="" />
               <img className=" blur-sm lg:scale-[65%] 2xl:scale-[100%] lg:translate-x-[1060%]  lg:translate-y-[360%] translate-x-[1400%] translate-y-[500%] absolute duration-700" src="/images/leaf 2.png" alt="" />
@@ -54,24 +48,20 @@ function App() {
           </div>
         </div>
         {/* others */}
-        <div>
-
-        </div>
+        <div></div>
       </div>
 
       {/* content principal */}
-      <div className={
-        isLoad ? 'opacity-0 duration-1000 ease-in-out' : 'opacity-100 duration-1000 ease-in-out'
-      }>
-        <div className='absolute w-full'>
+      <div className={isLoad ? "opacity-0 duration-1000 ease-in-out" : "opacity-100 duration-1000 ease-in-out"}>
+        <div className="absolute w-full">
           <Navbar />
         </div>
-        <div className='!z-50'>
+        <div className="!z-50">
           <Base />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -11,43 +11,20 @@ export default function Pizza() {
         <div className="flex gap-8 p-4">
           {/* Prev */}
           <div className="flex">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              zoomAndPan="magnify"
-              viewBox="0 0 75 37.5"
-              preserveAspectRatio="xMidYMid meet"
-              version="1.0"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" zoomAndPan="magnify" viewBox="0 0 75 37.5" preserveAspectRatio="xMidYMid meet" version="1.0">
               <defs>
                 <clipPath id="a17eb4f0a0">
-                  <path
-                    d="M 25.800781 0 L 48.003906 0 L 48.003906 37 L 25.800781 37 Z M 25.800781 0 "
-                    clip-rule="nonzero"
-                  />
+                  <path d="M 25.800781 0 L 48.003906 0 L 48.003906 37 L 25.800781 37 Z M 25.800781 0 " clip-rule="nonzero" />
                 </clipPath>
                 <clipPath id="82fd7f86fb">
-                  <path
-                    d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 "
-                    clip-rule="nonzero"
-                  />
+                  <path d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 " clip-rule="nonzero" />
                 </clipPath>
               </defs>
               <g clip-path="url(#a17eb4f0a0)">
-                <path
-                  fill="#ffffff"
-                  d="M 44.128906 36.964844 L 25.804688 18.5625 L 44.257812 0.0351562 L 48.007812 3.800781 L 33.308594 18.5625 L 47.882812 33.199219 L 44.128906 36.964844 "
-                  fill-opacity="1"
-                  fill-rule="nonzero"
-                />
+                <path fill="#ffffff" d="M 44.128906 36.964844 L 25.804688 18.5625 L 44.257812 0.0351562 L 48.007812 3.800781 L 33.308594 18.5625 L 47.882812 33.199219 L 44.128906 36.964844 " fill-opacity="1" fill-rule="nonzero" />
               </g>
               <g clip-path="url(#82fd7f86fb)">
-                <path
-                  fill="#ffffff"
-                  d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
-                  fill-opacity="1"
-                  fill-rule="nonzero"
-                />
+                <path fill="#ffffff" d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 " fill-opacity="1" fill-rule="nonzero" />
               </g>
             </svg>
             Prev.
@@ -55,48 +32,22 @@ export default function Pizza() {
           {/* Next */}
           <div className="flex scale-105 font-semibold">
             Next
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-
-
-              zoomAndPan="magnify"
-              viewBox="0 0 75 37.5"
-              className="w-6 h-6"
-              preserveAspectRatio="xMidYMid meet"
-              version="1.0"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" zoomAndPan="magnify" viewBox="0 0 75 37.5" className="w-6 h-6" preserveAspectRatio="xMidYMid meet" version="1.0">
               <defs>
                 <clipPath id="9ff828e112">
-                  <path
-                    d="M 26.445312 0 L 48.648438 0 L 48.648438 37 L 26.445312 37 Z M 26.445312 0 "
-                    clip-rule="nonzero"
-                  />
+                  <path d="M 26.445312 0 L 48.648438 0 L 48.648438 37 L 26.445312 37 Z M 26.445312 0 " clip-rule="nonzero" />
                 </clipPath>
                 <clipPath id="3ea57380e6">
-                  <path
-                    d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 "
-                    clip-rule="nonzero"
-                  />
+                  <path d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 " clip-rule="nonzero" />
                 </clipPath>
               </defs>
               <g clip-path="url(#9ff828e112)">
-                <path
-                  fill="#ffffff"
-                  d="M 30.320312 0.0390625 L 48.644531 18.441406 L 30.191406 36.972656 L 26.441406 33.203125 L 41.140625 18.441406 L 26.566406 3.808594 L 30.320312 0.0390625 "
-                  fill-opacity="1"
-                  fill-rule="nonzero"
-                />
+                <path fill="#ffffff" d="M 30.320312 0.0390625 L 48.644531 18.441406 L 30.191406 36.972656 L 26.441406 33.203125 L 41.140625 18.441406 L 26.566406 3.808594 L 30.320312 0.0390625 " fill-opacity="1" fill-rule="nonzero" />
               </g>
               <g clip-path="url(#3ea57380e6)">
-                <path
-                  fill="#ffffff"
-                  d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
-                  fill-opacity="1"
-                  fill-rule="nonzero"
-                />
+                <path fill="#ffffff" d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 " fill-opacity="1" fill-rule="nonzero" />
               </g>
             </svg>
-
           </div>
         </div>
       </div>
@@ -117,14 +68,7 @@ export default function Pizza() {
 
           <div className="flex flex-col gap-3 justify-center">
             <div className="line-through text-warning">$7.90</div>
-            <CountUp
-              delay={0}
-              start={0}
-              end={7.9}
-              duration={4}
-              decimals={2}
-              decimal=","
-            >
+            <CountUp delay={0} start={0} end={7.9} duration={4} decimals={2} decimal=",">
               {({ countUpRef }) => (
                 <div className="text-2xl lg:text-3xl  ">
                   $<span ref={countUpRef} className="animate-bounce infinite" />
@@ -136,11 +80,7 @@ export default function Pizza() {
           <div className="flex justify-center items-center">
             <div className="flex items-center justify-center gap-2 btn btn-warning rounded-full lowercase">
               <div>
-                <svg
-                  className="w-7 h-7 stroke-black fill-none"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg className="w-7 h-7 stroke-black fill-none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <defs></defs>
 
                   <g id="ic-actions-timer">
