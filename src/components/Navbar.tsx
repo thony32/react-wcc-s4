9b7c4abefb53d734cfc615929eb87b08889ced5b
Raya <raya@pop-os.localdomain>
2023-07-22 14:03:49 +0300

    e

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,35 +11,18 @@ export default function Navbar() {
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
               <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
               <li><a>Item 3</a></li>
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className=" menu-horizontal">
-            <li className="hover:border-t-2 hover:border-l-2 hover:rounded-tl-lg p-2"><a className="">Item 1</a></li>
-            <li tabIndex={0}>
-              <details>
-                <summary>Menu</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
+          <a className="font-bold text-2xl">
+            Fode<span className="text-[#ffdb80] text-4xl">.</span>
+          </a>
         </div>
         <div className="navbar-end">
-          <a className="btn btn-outline rounded-full hover:bg-[#ffdb81] lowercase capitalize px-5">Sign up</a>
-          <a className="btn rounded-full bg-[#ffdb80] hover:bg-[#ffdb81] lowercase capitalize px-7">Login</a>
+          <a className="hover:border-t-2 hover:border-l-2 hover:rounded-tl-lg p-2 mx-4">Item 1</a>
+          <a className="hover:border-t-2 hover:border-l-2 hover:rounded-tl-lg p-2 mx-10">Item 1</a>
+          <a className="btn btn-outline rounded-full hover:bg-[#ffdb80] hover:border-[#ffdb80] hover:text-black lowercase capitalize px-5 mx-4">Sign up</a>
+          <a className="btn rounded-full bg-[#ffdb80] hover:bg-[#ffdb81] lowercase capitalize px-7 ">Login</a>
         </div>
       </div>
     </div>
