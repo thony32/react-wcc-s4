04688eddf53e84392bf85e51def1aa6ddacf9320
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-22 19:09:19 +0300

    ambany

--- src/components/Chef.tsx
+++ src/components/Chef.tsx
@@ -1,6 +1,58 @@
 export default function Chef() {
   return (
-    <div className="flex items-center gap-8 p-8 border-l-4 border-t-4  border-l-dashed border border-l-grey-400 rounded-l-full">
+    <div className="relative flex items-center gap-8 p-8 border-l-4  border-dashed border-l-grey-400 rounded-l-full translate-x-[100px]">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute top-0 right-0 w-6 h-6"
+        zoomAndPan="magnify"
+        viewBox="0 0 30 30.000001"
+        preserveAspectRatio="xMidYMid meet"
+        version="1.0"
+      >
+        <defs>
+          <clipPath id="ed3fb0c392">
+            <path
+              d="M 0.484375 0 L 29.515625 0 L 29.515625 29.03125 L 0.484375 29.03125 Z M 0.484375 0 "
+              clip-rule="nonzero"
+            />
+          </clipPath>
+        </defs>
+        <g clip-path="url(#ed3fb0c392)">
+          <path
+            fill="gray"
+            d="M 28.070312 29.03125 C 27.273438 29.03125 26.625 28.386719 26.625 27.585938 C 26.625 13.96875 15.546875 2.890625 1.929688 2.890625 C 1.132812 2.890625 0.484375 2.242188 0.484375 1.445312 C 0.484375 0.648438 1.132812 0 1.929688 0 C 5.652344 0 9.265625 0.726562 12.667969 2.167969 C 15.953125 3.558594 18.902344 5.546875 21.433594 8.078125 C 23.964844 10.609375 25.953125 13.5625 27.34375 16.847656 C 28.785156 20.246094 29.511719 23.859375 29.511719 27.585938 C 29.515625 28.386719 28.867188 29.03125 28.070312 29.03125 Z M 28.070312 29.03125 "
+            fill-opacity="1"
+            fill-rule="nonzero"
+          />
+        </g>
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        
+        className="absolute bottom-0 right-0 w-6 h-6"
+        zoomAndPan="magnify"
+        viewBox="0 0 30 30.000001"
+        
+        preserveAspectRatio="xMidYMid meet"
+        version="1.0"
+      >
+        <defs>
+          <clipPath id="a4cd3f9139">
+            <path
+              d="M 0.484375 0 L 29.515625 0 L 29.515625 29.03125 L 0.484375 29.03125 Z M 0.484375 0 "
+              clip-rule="nonzero"
+            />
+          </clipPath>
+        </defs>
+        <g clip-path="url(#a4cd3f9139)">
+          <path
+            fill="gray"
+            d="M 0.484375 27.585938 C 0.484375 26.789062 1.132812 26.140625 1.929688 26.140625 C 15.546875 26.140625 26.625 15.0625 26.625 1.445312 C 26.625 0.648438 27.273438 0 28.070312 0 C 28.867188 0 29.515625 0.648438 29.515625 1.445312 C 29.515625 5.167969 28.789062 8.78125 27.347656 12.183594 C 25.957031 15.46875 23.96875 18.417969 21.4375 20.949219 C 18.90625 23.484375 15.953125 25.472656 12.671875 26.863281 C 9.269531 28.300781 5.65625 29.03125 1.933594 29.03125 C 1.132812 29.03125 0.484375 28.386719 0.484375 27.585938 Z M 0.484375 27.585938 "
+            fill-opacity="1"
+            fill-rule="nonzero"
+          />
+        </g>
+      </svg>
       <div>sary</div>
       <div>
         <div className="text-3xl">Chef's Recommendation</div>
