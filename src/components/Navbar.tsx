32d6fdae034eb749b858d7ba886ea79f758bdbb1
Raya <raya@pop-os.localdomain>
2023-07-22 11:38:10 +0300

    first

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,47 @@
+import "../style/navbar.css";
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><a>Item 1</a></li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className=" menu-horizontal">
+            <li className="hover:border-t-2 hover:border-l-2 hover:rounded-tl-lg p-2"><a className="">Item 1</a></li>
+            <li tabIndex={0}>
+              <details>
+                <summary>Menu</summary>
+                <ul className="p-2">
+                  <li><a>Submenu 1</a></li>
+                  <li><a>Submenu 2</a></li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-outline rounded-full hover:bg-[#ffdb81] lowercase capitalize px-5">Sign up</a>
+          <a className="btn rounded-full bg-[#ffdb80] hover:bg-[#ffdb81] lowercase capitalize px-7">Login</a>
+        </div>
+      </div>
+    </div>
   )
 }
