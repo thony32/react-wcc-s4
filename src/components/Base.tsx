62fe3ccf398df9f3279ccfa9fc26cec7fee86876
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-22 15:03:34 +0300

    zezika ny adve

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,12 +1,13 @@
 import Pizza from "./Pizza";
+import Chef from "./Chef";
 
 export default function Base() {
-    return (
-        <div>
-            
-            <div>
-                <Pizza />
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="flex p-16 justify-between">
+        <Chef />
+        <Pizza />
+      </div>
+    </div>
+  );
 }
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -10,9 +10,94 @@ export default function Pizza() {
         </div>
         <div className="flex gap-8 p-4">
           {/* Prev */}
-          <div className="flex gap-2">Prev.</div>
+          <div className="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              zoomAndPan="magnify"
+              viewBox="0 0 75 37.5"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+            >
+              <defs>
+                <clipPath id="a17eb4f0a0">
+                  <path
+                    d="M 25.800781 0 L 48.003906 0 L 48.003906 37 L 25.800781 37 Z M 25.800781 0 "
+                    clip-rule="nonzero"
+                  />
+                </clipPath>
+                <clipPath id="82fd7f86fb">
+                  <path
+                    d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 "
+                    clip-rule="nonzero"
+                  />
+                </clipPath>
+              </defs>
+              <g clip-path="url(#a17eb4f0a0)">
+                <path
+                  fill="#000000"
+                  d="M 44.128906 36.964844 L 25.804688 18.5625 L 44.257812 0.0351562 L 48.007812 3.800781 L 33.308594 18.5625 L 47.882812 33.199219 L 44.128906 36.964844 "
+                  fill-opacity="1"
+                  fill-rule="nonzero"
+                />
+              </g>
+              <g clip-path="url(#82fd7f86fb)">
+                <path
+                  fill="#000000"
+                  d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
+                  fill-opacity="1"
+                  fill-rule="nonzero"
+                />
+              </g>
+            </svg>
+            Prev.
+          </div>
           {/* Next */}
-          <div className="flex gap-2">Next</div>
+          <div className="flex scale-125 font-semibold">
+            Next
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              
+              
+              zoomAndPan="magnify"
+              viewBox="0 0 75 37.5"
+              className="w-6 h-6"
+              preserveAspectRatio="xMidYMid meet"
+              version="1.0"
+            >
+              <defs>
+                <clipPath id="9ff828e112">
+                  <path
+                    d="M 26.445312 0 L 48.648438 0 L 48.648438 37 L 26.445312 37 Z M 26.445312 0 "
+                    clip-rule="nonzero"
+                  />
+                </clipPath>
+                <clipPath id="3ea57380e6">
+                  <path
+                    d="M 26.351562 17.792969 L 47.816406 17.792969 L 47.816406 19.273438 L 26.351562 19.273438 Z M 26.351562 17.792969 "
+                    clip-rule="nonzero"
+                  />
+                </clipPath>
+              </defs>
+              <g clip-path="url(#9ff828e112)">
+                <path
+                  fill="#000000"
+                  d="M 30.320312 0.0390625 L 48.644531 18.441406 L 30.191406 36.972656 L 26.441406 33.203125 L 41.140625 18.441406 L 26.566406 3.808594 L 30.320312 0.0390625 "
+                  fill-opacity="1"
+                  fill-rule="nonzero"
+                />
+              </g>
+              <g clip-path="url(#3ea57380e6)">
+                <path
+                  fill="#000000"
+                  d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
+                  fill-opacity="1"
+                  fill-rule="nonzero"
+                />
+              </g>
+            </svg>
+            
+          </div>
         </div>
       </div>
       {/* Pizza */}
@@ -24,7 +109,10 @@ export default function Pizza() {
 
           <div className="col-span-2 flex flex-col gap-4 items-center lg:items-start justify-center">
             <div className=" lg:text-2xl text-left">Mexicoo Pizza</div>
-            <div className="text-sm">78 Calories</div>
+            <div className="text-sm flex gap-2 items-center">
+              <div className="loader"></div>
+              78 Calories
+            </div>
           </div>
 
           <div className="flex flex-col gap-3 justify-center">
@@ -47,19 +135,21 @@ export default function Pizza() {
 
           <div className="flex justify-center items-center">
             <div className="flex items-center justify-center gap-2 btn btn-warning rounded-full lowercase">
-              <div><svg
-                className="w-7 h-7 stroke-black fill-none"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <defs></defs>
+              <div>
+                <svg
+                  className="w-7 h-7 stroke-black fill-none"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs></defs>
 
-                <g id="ic-actions-timer">
-                  <circle className="cls-1" cx="12" cy="13.5" r="8" />
+                  <g id="ic-actions-timer">
+                    <circle className="cls-1" cx="12" cy="13.5" r="8" />
 
-                  <polyline className="cls-2" points="12 9 12 14 15 16" />
-                </g>
-              </svg></div>
+                    <polyline className="cls-2" points="12 9 12 14 15 16" />
+                  </g>
+                </svg>
+              </div>
               <div className="text-center text-lg">30 dk.</div>
             </div>
           </div>
