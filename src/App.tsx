9cddaf57c2c65cff0b4d653f83da2500529df693
Rayounette <murielarisoaran@gmail.com>
2023-07-22 16:10:34 +0300

    init image

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import Navbar from './components/Navbar'
 function App() {
   const [isLoad, setIsLoad] = useState(true)
 
-  const [valia , setValia] = useState("scale-[30%] translate-x-[200%] translate-y-[25%] absolute duration-700")
-  const [baguette_deux , setBaguette_deux] = useState("scale-[30%] translate-x-[160%] translate-y-[75%] rotate-[20deg] z-10 absolute duration-700")
-  const [baguette_un , setBaguette_un] = useState("scale-[30%] translate-x-[150%] translate-y-[75%] rotate-[90deg] z-10 absolute duration-700")
-  const [vanttan , setVanttan] = useState("scale-[20%] translate-x-[255%] translate-y-[65%] z-0 absolute duration-700")
+  const [valia, setValia] = useState("scale-[30%] translate-x-[200%] translate-y-[25%] absolute duration-700")
+  const [baguette_deux, setBaguette_deux] = useState("scale-[30%] translate-x-[160%] translate-y-[75%] rotate-[20deg] z-10 absolute duration-700")
+  const [baguette_un, setBaguette_un] = useState("scale-[30%] translate-x-[150%] translate-y-[75%] rotate-[90deg] z-10 absolute duration-700")
+  const [vanttan, setVanttan] = useState("scale-[20%] translate-x-[255%] translate-y-[65%] z-0 absolute duration-700")
 
 
   useEffect(() => {
@@ -35,10 +35,20 @@ function App() {
             {/* vilia */}
             <img className={valia} src="/images/vilia.png" alt="" />
             {/* bagette */}
-            <img className={baguette_deux} src="/public/images/baguette 2.png" alt="" />
-            <img className={baguette_un} src="/public/images/baguette 1.png" alt="" />
+            <img className={baguette_deux} src="/images/baguette 2.png" alt="" />
+            <img className={baguette_un} src="/images/baguette 1.png" alt="" />
             {/* vantan */}
             <img className={vanttan} src="/images/Vanttan.png" alt="" />
+
+            {/* Others */}
+            <div className={
+              isLoad ? 'opacity-0 duration-1000 ease-in-out' : 'opacity-100 duration-1000 ease-in-out'
+            }>
+              <img className="translate-x-[700%] blur-sm translate-y-[125%] absolute duration-700" src="/images/tomate.png" alt="" />
+              <img className="translate-x-[700%] blur-sm translate-y-[75%] absolute duration-700" src="/images/leaf 1.png" alt="" />
+              <img className="translate-x-[1400%] blur-sm translate-y-[500%] absolute duration-700" src="/images/leaf 2.png" alt="" />
+              <img className="translate-x-[1400%] blur-sm translate-y-[750%] absolute duration-700" src="/images/oignon.png" alt="" />
+            </div>
           </div>
         </div>
         {/* others */}
