837296663e46acccdc582d5b0f5d3d78e555e12b
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-23 14:14:30 +0300

    namboariko lay fleche

--- src/components/Chef.tsx
+++ src/components/Chef.tsx
@@ -29,7 +29,7 @@ export default function Chef() {
         <div className="lg:text-lg 2xl:text-3xl">Chef's Recommendation</div>
         <div className="flex gap-2 lg:text-lg 2xl:text-3xl font-bold">
           See the menu types
-          <svg viewBox="0 0 75 37.5" className="w-9 h-9 group-hover:translate-x-2 duration-300">
+          <svg viewBox="0 0 75 37.5" className="w-9 h-9 rotate-90 group-hover:translate-y-2 lg:rotate-0 lg:group-hover:translate-x-2 lg:group-hover:translate-y-0 duration-300">
             <g clipPath="url(#9ff828e112)">
               <path
                 fill="#000000"
