9f927bb0c4b3189bdc3a2ce8e8bfa73ca75ca1eb
MurielArii <murielarisoaran@gmail.com>
2023-07-23 11:44:57 +0300

    button sign in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,17 +21,17 @@ export default function Navbar() {
     document.getElementById("baguette_two")?.classList.remove("rotate-[90deg]")
   });
 
-  about?.addEventListener("mouseover" ,() => {
+  about?.addEventListener("mouseover", () => {
     document.getElementById("vantan")?.classList.add("!scale-[130%]")
   })
-  about?.addEventListener("mouseout" ,() => {
+  about?.addEventListener("mouseout", () => {
     document.getElementById("vantan")?.classList.remove("!scale-[130%]")
   })
 
-  menu?.addEventListener("mouseover" , () => {
+  menu?.addEventListener("mouseover", () => {
     document.getElementById("others")?.classList.add("!opacity-0")
   })
-  menu?.addEventListener("mouseout" , () => {
+  menu?.addEventListener("mouseout", () => {
     document.getElementById("others")?.classList.remove("!opacity-0")
   })
   return (
@@ -87,7 +87,10 @@ export default function Navbar() {
             </button>
           </div>
           <div className="flex gap-3 lg:gap-3 lg:translate-x-5 2xl:translate-x-0">
-            <button className="btn btn-outline border-white text-white hover:text-[#ffdb81] hover:bg-transparent hover:border-[#ffdb81] rounded-full capitalize 2xl:px-6">Sign up</button>
+            <button className="group relative min-h-[50px] w-20 overflow-hidden border border-white text-white rounded-full shadow-2xl transition-all before:absolute before:left-0 before:top-0 before:h-0 before:w-1/4 before:bg-[#ffdb80] before:duration-500 after:absolute after:bottom-0 after:right-0 after:h-0 after:w-1/4 after:bg-[#ffdb80] after:duration-500 hover:text-black hover:before:h-full hover:after:h-full">
+              <span className="top-0 flex h-full w-full items-center justify-center before:absolute before:bottom-0 before:left-1/4 before:z-0 before:h-0 before:w-1/4 before:bg-[#ffdb80] before:duration-500 after:absolute after:right-1/4 after:top-0 after:z-0 after:h-0 after:w-1/4 after:bg-[#ffdb80] after:duration-500 hover:text-black group-hover:before:h-full group-hover:after:h-full"></span>
+              <span className="absolute bottom-0 left-0 right-0 top-0 z-10 flex h-full w-full items-center justify-center group-hover:text-black">Sign up</span>
+            </button>
             <button className="btn rounded-full bg-[#ffdb80] hover:bg-[#ffdb81] capitalize 2xl:px-6">Login</button>
           </div>
         </div>
