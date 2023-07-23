5c8898293c07de1aabc79646b5a7af1752025361
Rayounette <murielarisoaran@gmail.com>
2023-07-23 09:50:02 +0300

    fix 4

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -3,9 +3,9 @@ import pizza from "../assets/pizza.png";
 
 const Pizza = () => {
   return (
-    <div className="lg:w-[40%] 2xl:w-1/3  text-black lg:text-white">
+    <div className="w-1/3 text-white">
       {/* Discount coupon */}
-      <div className="flex border-dashed border-black lg:border-white border border-b-0 rounded-t-2xl justify-between p-4">
+      <div className="flex border-dashed border-white border border-b-0 rounded-t-2xl justify-between p-4">
         <div className="p-4 font-semibold">Discount Coupon</div>
         <div className="flex gap-8 p-4">
           {/* Prev */}
@@ -16,7 +16,7 @@ const Pizza = () => {
             >
               <g clipPath="url(#a17eb4f0a0)">
                 <path
-                  className="fill-black lg:fill-white"
+                  fill="#ffffff"
                   d="M 44.128906 36.964844 L 25.804688 18.5625 L 44.257812 0.0351562 L 48.007812 3.800781 L 33.308594 18.5625 L 47.882812 33.199219 L 44.128906 36.964844 "
                   fillOpacity="1"
                   fillRule="nonzero"
@@ -24,7 +24,7 @@ const Pizza = () => {
               </g>
               <g clipPath="url(#82fd7f86fb)">
                 <path
-                  className="fill-black lg:fill-white"
+                  fill="#ffffff"
                   d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
                   fillOpacity="1"
                   fillRule="nonzero"
@@ -39,7 +39,7 @@ const Pizza = () => {
             <svg viewBox="0 0 75 37.5" className="w-6 h-6 group-hover:translate-x-2 duration-300">
               <g clipPath="url(#9ff828e112)">
                 <path
-                  className="fill-black lg:fill-white"
+                  fill="#ffffff"
                   d="M 30.320312 0.0390625 L 48.644531 18.441406 L 30.191406 36.972656 L 26.441406 33.203125 L 41.140625 18.441406 L 26.566406 3.808594 L 30.320312 0.0390625 "
                   fillOpacity="1"
                   fillRule="nonzero"
@@ -47,7 +47,7 @@ const Pizza = () => {
               </g>
               <g clipPath="url(#3ea57380e6)">
                 <path
-                  className="fill-black lg:fill-white"
+                  fill="#ffffff"
                   d="M 46.914062 17.792969 L 27.253906 17.792969 C 26.867188 17.792969 26.550781 18.121094 26.550781 18.53125 C 26.550781 18.941406 26.867188 19.273438 27.253906 19.273438 L 46.914062 19.273438 C 47.300781 19.273438 47.617188 18.941406 47.617188 18.53125 C 47.617188 18.121094 47.300781 17.792969 46.914062 17.792969 Z M 46.914062 17.792969 "
                   fillOpacity="1"
                   fillRule="nonzero"
@@ -58,8 +58,8 @@ const Pizza = () => {
         </div>
       </div>
       {/* Pizza */}
-      <div className="p-4 border border-dashed border-black lg:border-white rounded-2xl -translate-y-[10%]">
-        <div className="flex flex-wrap justify-between lg:grid lg:grid-cols-6">
+      <div className="p-4 border border-dashed border-white rounded-2xl -translate-y-[10%]">
+        <div className="grid grid-cols-6">
           <div>
             <img src={pizza} alt="" className="w-16 2xl:w-20" />
           </div>
@@ -84,7 +84,7 @@ const Pizza = () => {
           </div>
 
           <div className="flex col-span-2 justify-center items-center">
-            <button className="flex items-center justify-center gap-2 btn lg:btn-sm 2xl:btn-md bg-[#ffdb80] hover:bg-[#ffdb81] rounded-full lowercase">
+            <button className="flex items-center justify-center gap-2 btn lg:btn-sm 2xl:btn-md bg-[#ffdb80] rounded-full lowercase">
               <div>
                 <svg className="lg:w-6 lg:h-6 2xl:w-7 2xl:h-7 stroke-black fill-none" viewBox="0 0 24 24">
                   <defs></defs>
